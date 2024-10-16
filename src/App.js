219a5cb0870589c9ff3a8f9b9e88ae4f784d684a
olga.olegovna.603 <olga.olegovna.603@list.ru>
2024-10-16 20:29:06 +0300

    new

--- src/App.js
+++ src/App.js
@@ -1,25 +1,8 @@
 
-import React,{useState} from "react";
-import { data } from "./data";
-import { CardItem } from "./CardItem";
-import {UserForm} from "./UserForm";
+import React from "react";
+
 
 
 export const App= () => {
-  const[persons, setPersons] = useState(data);
-  
-  const onDeletePerson=(personId)=>{
-    console.log(personId)
-    setPersons((prevPersons)=>
-      prevPersons.filter((person)=>person.id!==personId));
-};
-const onUserAdd=(userObj)=>{
-  setPersons((prevPersons)=>[...prevPersons,userObj])
-};
-return<>
-<div>hello,world</div>
-<UserForm onUserAdd={onUserAdd}/>
-{persons.map((person)=> 
-<CardItem {...person} key={person.id} onDeletePerson={()=>onDeletePerson(person.id)}/>)}
-</>
+  return<div>hello world</div>
 };
